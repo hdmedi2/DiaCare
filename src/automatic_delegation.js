cdb00ce68fe22611fa107383ea59421281b5e8f3
sungjin-Kim11 <plplplplo48@gmail.com>
2024-08-23 11:02:56 +0900

    주석 해제

--- src/automatic_delegation.js
+++ src/automatic_delegation.js
@@ -97,104 +97,104 @@ async function runAutomation_delegation(data_1) {
     await frame.locator('#inp_mdtToDt_input').fill('');
     await frame.locator('#inp_mdtToDt_input').fill(data_1.end.replace(/-/g, ''));
 
-    // // 저장 버튼
-    // await frame.getByRole('link', { name: '저장' }).click();
-    // console.log("Click the Save button");
+    // 저장 버튼
+    await frame.getByRole('link', { name: '저장' }).click();
+    console.log("Click the Save button");
 
-    // // 알림창
-    // const frames_save = page.frames();
-    // let dynamicFrame_save;
-    // let dynamicFrameId_save;
-    // for (let i = frames_save.length - 1; i >= 0; i--) {
-    //     const frame_save = frames_save[i];
-    //     await page.waitForTimeout(1000);
-    //     const ids_save = await frame_save.evaluate(() => {
-    //         const iframes = Array.from(document.querySelectorAll('iframe'));
-    //         return iframes.map(iframe => iframe.id);
-    //     });
-    //     for (const id_save of ids_save) {
-    //         if (id_save.startsWith('confirm_') && id_save.endsWith('_iframe')) {
-    //             dynamicFrame_save = frame_save;
-    //             dynamicFrameId_save = id_save;
-    //             console.log('Dynamic iframe found with ID:', id_save);
-    //             break;
-    //         }
-    //     }
-    //     if (dynamicFrame_save) break;
-    // }
-    // if (dynamicFrame_save && dynamicFrameId_save) {
-    //     const innerFrame_save = dynamicFrame_save.frameLocator(`iframe[id="${dynamicFrameId_save}"]`);
-    //     await innerFrame_save.getByRole('link', { name: '예' }).waitFor();
-    //     await innerFrame_save.getByRole('link', { name: '예' }).click();
-    // } else {
-    //     console.error('Dynamic iframe not found.');
-    // }
+    // 알림창
+    const frames_save = page.frames();
+    let dynamicFrame_save;
+    let dynamicFrameId_save;
+    for (let i = frames_save.length - 1; i >= 0; i--) {
+        const frame_save = frames_save[i];
+        await page.waitForTimeout(1000);
+        const ids_save = await frame_save.evaluate(() => {
+            const iframes = Array.from(document.querySelectorAll('iframe'));
+            return iframes.map(iframe => iframe.id);
+        });
+        for (const id_save of ids_save) {
+            if (id_save.startsWith('confirm_') && id_save.endsWith('_iframe')) {
+                dynamicFrame_save = frame_save;
+                dynamicFrameId_save = id_save;
+                console.log('Dynamic iframe found with ID:', id_save);
+                break;
+            }
+        }
+        if (dynamicFrame_save) break;
+    }
+    if (dynamicFrame_save && dynamicFrameId_save) {
+        const innerFrame_save = dynamicFrame_save.frameLocator(`iframe[id="${dynamicFrameId_save}"]`);
+        await innerFrame_save.getByRole('link', { name: '예' }).waitFor();
+        await innerFrame_save.getByRole('link', { name: '예' }).click();
+    } else {
+        console.error('Dynamic iframe not found.');
+    }
 
-    // // 알림창
-    // await page.waitForTimeout(2000);
-    // const frames_save_alert = page.frames();
-    // let dynamicFrame_save_alert;
-    // let dynamicFrameId_save_alert;
-    // for (let i = frames_save_alert.length - 1; i >= 0; i--) {
-    //     const frame_save_alert = frames_save_alert[i];
-    //     await page.waitForTimeout(1000);
-    //     const ids_save_alert = await frame_save_alert.evaluate(() => {
-    //         const iframes = Array.from(document.querySelectorAll('iframe'));
-    //         return iframes.map(iframe => iframe.id);
-    //     });
-    //     for (const id_save_alert of ids_save_alert) {
-    //         if (id_save_alert.startsWith('alert_') && id_save_alert.endsWith('_iframe')) {
-    //             dynamicFrame_save_alert = frame_save_alert;
-    //             dynamicFrameId_save_alert = id_save_alert;
-    //             console.log('Dynamic iframe found with ID:', id_save_alert);
-    //             break;
-    //         }
-    //     }
-    //     if (dynamicFrame_save_alert) break;
-    // }
-    // if (dynamicFrame_save_alert && dynamicFrameId_save_alert) {
-    //     const innerFrame_save_alert = dynamicFrame_save_alert.frameLocator(`iframe[id="${dynamicFrameId_save_alert}"]`);
-    //     await innerFrame_save_alert.getByRole('link', { name: '확인' }).waitFor();
-    //     await innerFrame_save_alert.getByRole('link', { name: '확인' }).click();
-    // } else {
-    //     console.error('Dynamic iframe not found.');
-    // }
+    // 알림창
+    await page.waitForTimeout(2000);
+    const frames_save_alert = page.frames();
+    let dynamicFrame_save_alert;
+    let dynamicFrameId_save_alert;
+    for (let i = frames_save_alert.length - 1; i >= 0; i--) {
+        const frame_save_alert = frames_save_alert[i];
+        await page.waitForTimeout(1000);
+        const ids_save_alert = await frame_save_alert.evaluate(() => {
+            const iframes = Array.from(document.querySelectorAll('iframe'));
+            return iframes.map(iframe => iframe.id);
+        });
+        for (const id_save_alert of ids_save_alert) {
+            if (id_save_alert.startsWith('alert_') && id_save_alert.endsWith('_iframe')) {
+                dynamicFrame_save_alert = frame_save_alert;
+                dynamicFrameId_save_alert = id_save_alert;
+                console.log('Dynamic iframe found with ID:', id_save_alert);
+                break;
+            }
+        }
+        if (dynamicFrame_save_alert) break;
+    }
+    if (dynamicFrame_save_alert && dynamicFrameId_save_alert) {
+        const innerFrame_save_alert = dynamicFrame_save_alert.frameLocator(`iframe[id="${dynamicFrameId_save_alert}"]`);
+        await innerFrame_save_alert.getByRole('link', { name: '확인' }).waitFor();
+        await innerFrame_save_alert.getByRole('link', { name: '확인' }).click();
+    } else {
+        console.error('Dynamic iframe not found.');
+    }
 
-    // // 제출 서류 첨부
-    // await frame.getByRole('link', { name: '제출 서류 첨부' }).click();
-    // // 파일 첨부
-    // const fileChooserPromise = page.waitForEvent("filechooser");
-    // await frame
-    //     .frameLocator('iframe[title="popup_fileUpload"]')
-    //     .frameLocator("#btrsFrame")
-    //     .getByRole("button", { name: "① 파일추가" })
-    //     .click();
-    // // 컴퓨터에서 파일 찾기
-    // const fileChooser = await fileChooserPromise;
-    // const userHomeDirectory = process.env.HOME || process.env.USERPROFILE;
-    // const downloadsDirectory = path.join(userHomeDirectory, "Downloads");
-    // await fileChooser.setFiles(path.join(downloadsDirectory, "hello.pdf"));
-    // // 파일 전송
-    // await frame
-    //     .frameLocator('iframe[title="popup_fileUpload"]')
-    //     .frameLocator("#btrsFrame")
-    //     .getByRole("button", { name: "② 파일전송" })
-    //     .click();
-    // await frame
-    //     .frameLocator('iframe[title="popup_fileUpload"]')
-    //     .frameLocator("#btrsFrame")
-    //     .locator('text=저장완료')
-    //     .waitFor({ timeout: 0 });  // 저장완료 text가 나타날때까지 무한 대기
-    // console.log('"저장완료" 텍스트가 프레임 내에 나타났습니다.');
-    // // 파일 저장    
-    // await frame
-    //     .frameLocator('iframe[title="popup_fileUpload"]')
-    //     .frameLocator('#btrsFrame')
-    //     .getByRole('button', { name: '③ 적 용' })
-    //     .click();
+    // 제출 서류 첨부
+    await frame.getByRole('link', { name: '제출 서류 첨부' }).click();
+    // 파일 첨부
+    const fileChooserPromise = page.waitForEvent("filechooser");
+    await frame
+        .frameLocator('iframe[title="popup_fileUpload"]')
+        .frameLocator("#btrsFrame")
+        .getByRole("button", { name: "① 파일추가" })
+        .click();
+    // 컴퓨터에서 파일 찾기
+    const fileChooser = await fileChooserPromise;
+    const userHomeDirectory = process.env.HOME || process.env.USERPROFILE;
+    const downloadsDirectory = path.join(userHomeDirectory, "Downloads");
+    await fileChooser.setFiles(path.join(downloadsDirectory, "hello.pdf"));
+    // 파일 전송
+    await frame
+        .frameLocator('iframe[title="popup_fileUpload"]')
+        .frameLocator("#btrsFrame")
+        .getByRole("button", { name: "② 파일전송" })
+        .click();
+    await frame
+        .frameLocator('iframe[title="popup_fileUpload"]')
+        .frameLocator("#btrsFrame")
+        .locator('text=저장완료')
+        .waitFor({ timeout: 0 });  // 저장완료 text가 나타날때까지 무한 대기
+    console.log('"저장완료" 텍스트가 프레임 내에 나타났습니다.');
+    // 파일 저장    
+    await frame
+        .frameLocator('iframe[title="popup_fileUpload"]')
+        .frameLocator('#btrsFrame')
+        .getByRole('button', { name: '③ 적 용' })
+        .click();
 
-    // // 최종제출
-    // await frame.getByRole('link', { name: '최종제출' }).click();
+    // 최종제출
+    await frame.getByRole('link', { name: '최종제출' }).click();
 
     // 최종 제출 하시겠습니까? 예 아니요 버튼 (confirm_ _iframe)
     // await browser.close();
