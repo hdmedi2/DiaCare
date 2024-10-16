10b2344a1d2fe1cb09b9e20c94db9eef9b45b4a3
ybsong94 <ybsong94@hdmedi.co.kr>
2024-10-16 10:07:23 +0900

    위임크롤링 데이터를 바로 백엔드(환자지키미서비스)로 보내서 업데이틑 하는 방식 테스트 삭제 (원상복구)

--- src/auto_checkDelegation.js
+++ src/auto_checkDelegation.js
@@ -1,7 +1,6 @@
 const { chromium } = require("playwright");
 const fs = require("fs");
 const { parse } = require("json2csv");
-const {sendCrawlingFileToServer} = require("./logUtil");
 
 async function checkDelegation(data) {
   const channels = [
@@ -101,7 +100,7 @@ async function checkDelegation(data) {
     if (responseBody.dl_tbbibo59) {
       console.log("Number of rows:", responseBody.dl_tbbibo59.length);
 
-      /*const filePos = "C:\\output.json";
+      const filePos = "C:\\output.json";
       const filePosRead = "file://output.json"
 
       fs.writeFileSync(
@@ -122,19 +121,7 @@ async function checkDelegation(data) {
             console.log(allText);
           }
         }
-      };*/
-
-      // 백엔드로 파일 요청 테스트
-      let stringifiedList = JSON.stringify(responseBody.dl_tbbibo59);
-      /*const formData = new FormData();
-
-      formData.append('stringifiedList', stringifiedList);*/
-
-      await sendCrawlingFileToServer(
-          stringifiedList,
-          data.csrfToken,
-          data.csrfHeader
-      );
+      };
 
       // CSV로 저장
       //const fields = Object.keys(responseBody.dl_tbbibo59[0]); // CSV 헤더로 사용될 필드명
