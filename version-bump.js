95008adc65bb4a9b122be729b1c682ca3513de11
서정현 <westwiz@hdmedi.co.kr>
2024-10-21 10:42:29 +0900

    npm make 수행 시 자동으로 package.json version이 0.0.01 씩 자동으로 버전이 증가되도록  수정 처리. package-lock.json의 버전은 package.json에 종속되도록 소스 변경

--- version-bump.js
+++ version-bump.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 // package.json 파일 경로
 const packageJsonPath = path.join(__dirname, 'package.json');
+const packageLockJsonPath = path.join(__dirname, 'package-lock.json');
 
 // package.json 읽기
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageLockJson = JSON.parse(fs.readFileSync(packageLockJsonPath, 'utf8'));
 
 // version을 0.0.01 증가시키는 함수
 function bumpVersion(version) {
@@ -25,7 +27,11 @@ function bumpVersion(version) {
 // version 증가
 packageJson.version = bumpVersion(packageJson.version);
 
+// packageLockJson 파일의 버전은 packageJson 과 동일하게 처리
+packageLockJson.version = packageJson.version;
+
 // package.json 파일에 쓰기
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(packageLockJsonPath, JSON.stringify(packageLockJson, null, 2));
 
 console.log(`Version bumped to ${packageJson.version}`);
